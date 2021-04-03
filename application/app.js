adaf9a88dd11cc12d58d5a3530056331db73ee31
abdelrahman <a.nasser@divvoice.de>
2021-04-03 13:50:22 +0200

    add text body parser

--- application/app.js
+++ application/app.js
@@ -3,13 +3,14 @@ import handlers from "./handlers.js";
 
 const app = express(); // initialize an express instance
 
+app.use(express.text()); // parse json req body
 app.use(express.json()); // parse json req body
 
 // GET endpoints
 app.get("/auth/users", handlers.getUsers);
-app.get("/auth/users/:id", handlers.getUserById);
+app.get("/auth/users", handlers.getUserById);
 app.get("/auth/companies", handlers.getCompanies);
-app.get("/auth/companies/:id", handlers.getCompanyById);
+app.get("/auth/companies", handlers.getCompanyById);
 
 // POST endpoints
 app.post("/auth/users/login", handlers.loginUser);
@@ -18,11 +19,11 @@ app.post("/auth/users/add", handlers.addUser);
 app.post("/auth/companies/add", handlers.addCompany);
 
 // PATCH endpoints
-app.patch("/auth/users/:id", handlers.editUser);
-app.patch("/auth/companies/:id", handlers.editCompany);
+app.patch("/auth/users", handlers.editUser);
+app.patch("/auth/companies", handlers.editCompany);
 
 // DELETE endpoints
-app.delete("/auth/users/:id", handlers.deleteUser);
-app.delete("/auth/companies/:id", handlers.deleteCompany);
+app.delete("/auth/users", handlers.deleteUser);
+app.delete("/auth/companies", handlers.deleteCompany);
 
 export default app;
