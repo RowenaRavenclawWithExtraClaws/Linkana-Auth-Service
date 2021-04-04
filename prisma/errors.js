36484f2091832ca01b564c7ca424b0ae24b78dd6
abdelrahman <a.nasser@divvoice.de>
2021-04-04 13:35:28 +0200

    export errors object

---
+++ prisma/errors.js
@@ -0,0 +1,16 @@
+// error codes for prisma client
+const errors = {
+  p2000: "value or values ar too long",
+  p2001: "record does not exist",
+  p2002: "this value cannot be repeated",
+  p2003: "forien key failed",
+  p2004: "there is no such constraint",
+  p2005: "value stored for this field is not valid",
+  p2006: "value stored for this field is not valid",
+  p2007: "data sent is not valid",
+  p2008: "error trying to fetch data",
+  p2009: "error trying to fetch data",
+  p2010: "error trying to fetch data",
+};
+
+export default errors;
