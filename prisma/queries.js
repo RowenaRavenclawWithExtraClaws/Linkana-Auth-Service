061e184eb4a201f73adfa155d1d0078de1723068
abdelrahman <a.nasser@divvoice.de>
2021-04-04 13:35:45 +0200

    handle popular errors that may occur with quering the database

--- prisma/queries.js
+++ prisma/queries.js
@@ -1,4 +1,5 @@
 import Prisma from "@prisma/client";
+import errors from "./errors.js";
 
 const { PrismaClient } = Prisma; // get prisma client constructor
 const prisma = new PrismaClient(); // initialize prisma client
@@ -6,45 +7,97 @@ const prisma = new PrismaClient(); // initialize prisma client
 const disconnectPrismaClient = async () => await prisma.$disconnect();
 
 // create new database record
-const createUserRecord = async (data) =>
-  await prisma.users.create({ data: data });
+const createUserRecord = async (data) => {
+  try {
+    return await prisma.users.create({ data: data });
+  } catch (err) {
+    return errors[err.code];
+  }
+};
 
-const createCompanyRecord = async (data) =>
-  await prisma.companies.create({ data: data });
+const createCompanyRecord = async (data) => {
+  try {
+    return await prisma.companies.create({ data: data });
+  } catch (err) {
+    return errors[err.code];
+  }
+};
 
 // get database records
-const getUserRecords = async () => await prisma.users.findMany();
+const getUserRecords = async () => {
+  try {
+    return await prisma.users.findMany();
+  } catch (err) {
+    return errors[err.code];
+  }
+};
 
-const getCompanyRecords = async () => await prisma.companies.findMany();
+const getCompanyRecords = async () => {
+  try {
+    return await prisma.companies.findMany();
+  } catch (err) {
+    return errors[err.code];
+  }
+};
 
 // get record by id
-const getUserRecordById = async (id) =>
-  await prisma.users.findUnique({
-    where: { id: id },
-  });
+const getUserRecordById = async (id) => {
+  try {
+    return await prisma.users.findUnique({
+      where: { id: id },
+    });
+  } catch (err) {
+    return errors[err.code];
+  }
+};
 
-const getCompanyRecordById = async (id) =>
-  await prisma.companies.findUnique({
-    where: { id: id },
-  });
+const getCompanyRecordById = async (id) => {
+  try {
+    return await prisma.companies.findUnique({
+      where: { id: id },
+    });
+  } catch (err) {
+    return errors[err.code];
+  }
+};
 
 // update record
-const updateUserRecord = async (id, data) =>
-  await prisma.users.update({ where: { id: id }, data: data });
+const updateUserRecord = async (id, data) => {
+  try {
+    return await prisma.users.update({ where: { id: id }, data: data });
+  } catch (err) {
+    return errors[err.code];
+  }
+};
 
-const updateCompanyRecord = async (id, data) =>
-  await prisma.companies.update({ where: { id: id }, data: data });
+const updateCompanyRecord = async (id, data) => {
+  try {
+    return await prisma.companies.update({ where: { id: id }, data: data });
+  } catch (err) {
+    return errors[err.code];
+  }
+};
 
 // delete record
-const deleteUserRecord = async (id) =>
-  prisma.users.delete({
-    where: { id: id },
-  });
+const deleteUserRecord = async (id) => {
+  try {
+    return prisma.users.delete({
+      where: { id: id },
+    });
+  } catch (err) {
+    return errors[err.code];
+  }
+};
 
-const deleteCompanyRecord = async (id) =>
-  prisma.companies.delete({
-    where: { id: id },
-  });
+const deleteCompanyRecord = async (id) => {
+  try {
+    return prisma.companies.delete({
+      where: { id: id },
+    });
+  } catch (err) {
+    return errors[err.code];
+  }
+};
 
 const queries = {
   disconnectPrisma: disconnectPrismaClient,
