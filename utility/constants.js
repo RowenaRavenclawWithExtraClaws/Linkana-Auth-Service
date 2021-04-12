04c39ac4c3426d8a59c98a79cbe88f27cc63854f
abdelrahman <a.nasser@divvoice.de>
2021-04-12 15:01:49 +0200

    read more env vars

--- utility/constants.js
+++ utility/constants.js
@@ -1,3 +1,8 @@
+import dotenv from "dotenv";
+
+// load .env file contnets in process.env
+dotenv.config();
+
 export const statusCodes = {
   ok: 200,
   created: 201,
@@ -12,3 +17,6 @@ export const accessTokenSecret = process.env.KID;
 export const jwksUri = process.env.JWKSURI;
 export const audience = process.env.AUDIENCE;
 export const issuer = process.env.ISSUER;
+export const authUrl = process.env.AUTHURL;
+export const clientID = process.env.CLIENTID;
+export const clientSecret = process.env.CLIENTSECRET;
