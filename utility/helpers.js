f5823f5db3aa1a0d293d4763fb0c73fc27abb5e5
abdelrahman <a.nasser@divvoice.de>
2021-04-23 07:23:54 +0200

    add more helper functions

--- utility/helpers.js
+++ utility/helpers.js
@@ -1,7 +1,20 @@
 import { tokenUserIndex } from "./constants.js";
+import aes from "crypto-js/aes.js";
+import enc from "crypto-js/enc-latin1.js";
 
-export const parseValidationToken = (token) =>
-  token.slice(tokenUserIndex, tokenUserIndex + Number(token[token.length - 1]));
+export const parseValidationToken = (token) => ({
+  username: token.slice(
+    tokenUserIndex,
+    tokenUserIndex + Number(token[token.length - 3])
+  ),
+  email: token.slice(
+    tokenUserIndex + Number(token[token.length - 3]),
+    tokenUserIndex +
+      Number(token[token.length - 3]) +
+      Number(token[token.length - 2]) * 10 +
+      Number(token[token.length - 1])
+  ),
+});
 
 // encrypt and decrypt user paswwords
 export const code = (function () {
@@ -12,9 +25,18 @@ export const code = (function () {
     },
     decryptMessage: (encryptedMessage = "", secretkey = "eemxxiii") => {
       var decryptedBytes = aes.decrypt(encryptedMessage, secretkey);
-      var decryptedMessage = decryptedBytes.toString(enc.Latin1);
+      var decryptedMessage = decryptedBytes.toString(enc);
 
       return decryptedMessage;
     },
   };
 })();
+
+// used mainly to delete password field from user objects
+export const deleteObjectsField = (objects, fields) =>
+  objects.map((obj) => {
+    fields.forEach((field) => {
+      delete obj[field];
+    });
+    return obj;
+  });
