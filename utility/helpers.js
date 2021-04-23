6b05f369ae267eac7956594ddcd1d3754eb38d37
abdelrahman <a.nasser@divvoice.de>
2021-04-23 05:26:15 +0200

    add encryption and decryption methods

--- utility/helpers.js
+++ utility/helpers.js
@@ -2,3 +2,19 @@ import { tokenUserIndex } from "./constants.js";
 
 export const parseValidationToken = (token) =>
   token.slice(tokenUserIndex, tokenUserIndex + Number(token[token.length - 1]));
+
+// encrypt and decrypt user paswwords
+export const code = (function () {
+  return {
+    encryptMessage: (messageToencrypt = "", secretkey = "eemxxiii") => {
+      var encryptedMessage = aes.encrypt(messageToencrypt, secretkey);
+      return encryptedMessage.toString();
+    },
+    decryptMessage: (encryptedMessage = "", secretkey = "eemxxiii") => {
+      var decryptedBytes = aes.decrypt(encryptedMessage, secretkey);
+      var decryptedMessage = decryptedBytes.toString(enc.Latin1);
+
+      return decryptedMessage;
+    },
+  };
+})();
