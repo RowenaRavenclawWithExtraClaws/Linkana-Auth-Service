92ad04bd0f561fcc23944b3743e20a667864fd1d
abdelrahman <a.nasser@divvoice.de>
2021-04-23 05:05:25 +0200

    fix typo in error codes

--- prisma/errors.js
+++ prisma/errors.js
@@ -1,16 +1,16 @@
 // error codes for prisma client
 const errors = {
-  p2000: "value or values ar too long",
-  p2001: "record does not exist",
-  p2002: "this value cannot be repeated",
-  p2003: "forien key failed",
-  p2004: "there is no such constraint",
-  p2005: "value stored for this field is not valid",
-  p2006: "value stored for this field is not valid",
-  p2007: "data sent is not valid",
-  p2008: "error trying to fetch data",
-  p2009: "error trying to fetch data",
-  p2010: "error trying to fetch data",
+  P2000: "value or values ar too long",
+  P2001: "record does not exist",
+  P2002: "this value cannot be repeated",
+  P2003: "forien key failed",
+  P2004: "there is no such constraint",
+  P2005: "value stored for this field is not valid",
+  P2006: "value stored for this field is not valid",
+  P2007: "data sent is not valid",
+  P2008: "error trying to fetch data",
+  P2009: "error trying to fetch data",
+  P2010: "error trying to fetch data",
 };
 
 export default errors;
